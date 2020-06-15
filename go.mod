26432e27b9f51531ac8e1592f42de3c3a8ae206e
likejehu <etqash@gmail.com>
2020-06-15 11:54:44 +0300

    added validation package

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.14
 require (
 	github.com/eaigner/jet v0.0.0-20140105131908-ae59b2cccfe3
 	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/go-playground/validator/v10 v10.3.0
 	github.com/pkg/errors v0.9.1
 )
---
+++ validation/validation.go
@@ -0,0 +1,48 @@
+package validation
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/likejehu/dearapril/models"
+)
+
+// Validate use a single instance of Validate, it caches struct info
+var validate *validator.Validate
+
+// ValidateStruct is for validating the struct
+func ValidateStruct(p *models.Project) {
+	validate = validator.New()
+	// returns nil or ValidationErrors ( []FieldError )
+	err := validate.Struct(p)
+	if err != nil {
+
+		// this check is only needed when your code could produce
+		// an invalid value for validation such as interface with nil
+		// value most including myself do not usually have code like this.
+		if _, ok := err.(*validator.InvalidValidationError); ok {
+			fmt.Println(err)
+			return
+		}
+
+		for _, err := range err.(validator.ValidationErrors) {
+
+			fmt.Println(err.Namespace())
+			fmt.Println(err.Field())
+			fmt.Println(err.StructNamespace())
+			fmt.Println(err.StructField())
+			fmt.Println(err.Tag())
+			fmt.Println(err.ActualTag())
+			fmt.Println(err.Kind())
+			fmt.Println(err.Type())
+			fmt.Println(err.Value())
+			fmt.Println(err.Param())
+			fmt.Println()
+		}
+
+		// from here you can create your own error messages in whatever language you wish
+		return
+	}
+
+	// save user to database
+}
