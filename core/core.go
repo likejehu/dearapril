bc10dc75c5fc01f90574df1860e10f225f125bb4
likejehu <etqash@gmail.com>
2020-06-15 14:05:32 +0300

    added some skeleton to core

--- core/core.go
+++ core/core.go
@@ -1,6 +1,10 @@
 package core
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/likejehu/dearapril/models"
+)
 
 //Controller is struct that has core functionality of the app
 type Controller struct {
@@ -9,6 +13,30 @@ type Controller struct {
 // SayHello just says hello
 func (c *Controller) SayHello() {
 	fmt.Println("hello!")
+}
+
+// CreateProject  creates new project
+func (c *Controller) CreateProject() (p *models.Project, err error) {
+	p = new(models.Project)
+	return p, nil
+}
+
+// UpdateProject  updates properties of  given project
+func (c *Controller) UpdateProject(id string) (p *models.Project, err error) {
+	p = new(models.Project)
+	return p, nil
+}
+
+// ReadProject  gets given project
+func (c *Controller) ReadProject(id string) (p *models.Project, err error) {
+	p = new(models.Project)
+	return p, nil
+}
+
+// DeleteProject  deletes given project
+func (c *Controller) DeleteProject(id string) (err error) {
+
+	return nil
 }
 
 // App is instance of Controller
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -14,24 +14,29 @@ type Handler struct {
 // Hello just says hello
 func (h *Handler) Hello(w http.ResponseWriter, r *http.Request) {
 	core.App.SayHello()
+	w.WriteHeader(http.StatusOK)
 }
 
 // CreateProject is for creating a new project
 func (h *Handler) CreateProject(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("project created!")
+	w.WriteHeader(http.StatusOK)
 }
 
 // ReadProject is for creating a new project
 func (h *Handler) ReadProject(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("this is ur project!")
+	w.WriteHeader(http.StatusOK)
 }
 
 // UpdateProject is for creating a new project
 func (h *Handler) UpdateProject(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("project updated!")
+	w.WriteHeader(http.StatusOK)
 }
 
 // DeleteProject is for creating a new project
 func (h *Handler) DeleteProject(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("project deleted!")
+	w.WriteHeader(http.StatusNoContent)
 }
