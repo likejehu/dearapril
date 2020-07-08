ea76e339f37cf25030fa85153314a0056d83bad9
likejehu <etqash@gmail.com>
2020-07-08 10:30:39 +0300

    added tags to models

--- models/models.go
+++ models/models.go
@@ -5,29 +5,29 @@ import "time"
 //Project must contain at least one column
 type Project struct {
 	ID          int
-	Name        string
-	Description string
+	Name        string `json:"name"  validate:"required"`
+	Description string `json:"description"  validate:"required"`
 }
 
 // Column is manageble by user
 type Column struct {
 	ID       int
-	Name     string
+	Name     string `json:"name"  validate:"required"`
 	Position string
 }
 
 // Task can be created only within a Column
 type Task struct {
 	ID          int
-	Name        string
-	Description string
+	Name        string `json:"name"  validate:"required"`
+	Description string `json:"description"  validate:"required"`
 	Position    string
 }
 
 // Comment can be created only within a Task
 type Comment struct {
 	ID   int
-	Name string
-	Text string
+	Name string `json:"name"  validate:"required"`
+	Text string `json:"text"  validate:"required"`
 	Date time.Time
 }
