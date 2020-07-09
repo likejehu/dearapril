2b109ad7ae44994dde51a83e5bfcbb7efd54b259
likejehu <etqash@gmail.com>
2020-07-09 21:07:28 +0300

    added mock for handlers_test

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -3,8 +3,10 @@ package handlers
 import (
 	"errors"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/likejehu/dearapril/handlers/mocks"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,7 +17,12 @@ var (
 func TestGetAllProjects(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
 		//setup
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		rec := httptest.NewRecorder()
 
+		mockApp := mocks.AppController{}
+		handler := &Handler{&mockApp}
+		handler.GetAllProjects(rec, req)
 		//assertions
 		assert.Equal(t, http.StatusOK, rec.Code)
 	})
