219ae997db133059bae2ad9f5979183eb01ed11d
likejehu <etqash@gmail.com>
2020-07-09 15:55:04 +0300

    added handlers_test file

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/go-playground/validator/v10 v10.3.0
 	github.com/likejehu/usrcnt v0.0.0-20200423160426-f97d47c098ab
 	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.5.1
 )
--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -2,8 +2,21 @@ package handlers
 
 import (
 	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var (
 	error500 = errors.New("shit happens")
 )
+
+func TestGetAllProjects(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		//setup
+
+		//assertions
+		assert.Equal(t, http.StatusOK, rec.Code)
+	})
+}
