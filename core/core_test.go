efe69cdd234d43d4db75da0a23a2f48af9e3cd47
likejehu <etqash@gmail.com>
2020-07-11 13:54:56 +0300

    added 2 test to core test

--- core/core_test.go
+++ core/core_test.go
@@ -16,34 +16,25 @@ var (
 	testProjectTwo  = &models.Project{2, "this is the two", "how are you doin?"}
 	testProjects    = []*models.Project{testProjectOne, testProjectTwo}
 
-	projectJSON = `{"name":"this is the zEro","description":"zero!"}
-	`
+	testColumnOne  = &models.Column{1, "columnOne", 1}
+	testColumnTwo  = &models.Column{2, "columnTwo", 2}
+	testColumnZero = &models.Column{ID: 0, Name: "", Position: 0}
+	testColumns    = []*models.Column{testColumnOne, testColumnTwo}
 
-	testColumnOne = &models.Column{1, "columnOne", 1}
-	testColumnTwo = &models.Column{2, "columnTwo", 2}
-	testColumns   = []*models.Column{testColumnOne, testColumnTwo}
-
-	columnJSON = `{"id":1, "name":"columnOne","position":1}
-	`
 	testTaskOne = &models.Task{1, "taskOne", "desc1", 1}
 	testTaskTwo = &models.Task{1, "taskTwo", "desc2", 2}
 	testTasks   = []*models.Task{testTaskOne, testTaskTwo}
-	taskJSON    = `{"id":1, "name":"taskOne","description":"desc1","position":1}
-	`
 
 	testCommentOne = &models.Comment{1, "this is text1", "Dec 1 13:05:05 CET 2020"}
 	testCommentTwo = &models.Comment{2, "this is text2", "Dec 2 15:07:08 CET 2020"}
 	testComments   = []*models.Comment{testCommentOne, testCommentTwo}
-	commentJSON    = `{"id":1, "text":"this is text1","date":"Dec 1 13:05:05 CET 2020"}
-	`
 )
 
-//Projects
+//PROJECTS
 
 func TestReadProjects(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
 		//setup
-
 		mockStore := mocks.Storer{}
 		testController := Controller{&mockStore}
 		mockStore.On("GetAllProjects").Return(testProjects, nil)
@@ -51,5 +42,20 @@ func TestReadProjects(t *testing.T) {
 		mockStore.AssertExpectations(t)
 		//assertions
 		assert.Equal(t, testProjects, projects)
+	})
+}
+
+func TestCreateProject(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		//setup
+		mockStore := mocks.Storer{}
+		testController := Controller{&mockStore}
+		mockStore.On("CreateProject", testProjectOne).Return(1, nil)
+		mockStore.On("CreateColumn", testColumnZero).Return(1, nil)
+		mockStore.On("CreateProjectColumns", 1, 1).Return(nil)
+		projectid, _ := testController.CreateProject(testProjectOne)
+		mockStore.AssertExpectations(t)
+		//assertions
+		assert.Equal(t, projectid, 1)
 	})
 }
